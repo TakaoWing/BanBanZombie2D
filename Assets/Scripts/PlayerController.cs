8f5e75402661300a8d186d2764217ef781e6c624
TakaoWing <tt.wing2838783@gmail.com>
2021-10-02 21:50:10 +0900

    Player Tarn Clicked Mouse Position

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Vector2 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // マウスがクリックされたなら
+        if (Input.GetMouseButtonDown(0)) {
+            // マウスの座標を取得
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // オブジェクトの向きをマウスの座標に向ける
+            this.transform.rotation = Quaternion.FromToRotation(Vector3.up, mousePos.normalized);
+        }
+    }
+}
